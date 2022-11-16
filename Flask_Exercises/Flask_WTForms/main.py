0cc36b987e46b6af5bddee0fb1752dc75bb10843
Mike <maikumendez@gmail.com>
2022-11-16 16:49:33 +0900

    added ongoing Flask WTForms exercise

---
+++ Flask_Exercises/Flask_WTForms/main.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
+
+
+class LoginForm(FlaskForm):
+    email = StringField(label='Email', validators=[DataRequired(), Email()])
+    password = PasswordField(label='Password', validators=[DataRequired(), Length(min=8)])
+    submit = SubmitField(label="Log In")
+
+
+app = Flask(__name__)
+app.secret_key = "random-secret"
+
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    login_form = LoginForm(meta={'csrf': False})
+    print(request.method)
+    print(login_form.validate_on_submit())
+    if request.method == "POST" and login_form.validate_on_submit():
+        if login_form.email.data == "admin@email.com" and login_form.password.data == "12345678":
+            print("Success")
+            return render_template('success.html')
+        else:
+            print("Failure")
+            return render_template('denied.html')
+    print("Login")
+    return render_template('login.html', form=login_form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
